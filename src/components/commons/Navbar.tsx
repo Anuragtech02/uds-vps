1467bb49d4492317a38b3031913f22e526757b1a
Adarsh <adarshmishra0701@gmail.com>
2024-09-03 20:59:16 +0530

    drop down menu

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import logo from '@/assets/img/logo.png';
 import Button from './Button';
 import Link from 'next/link';
+import industries from '@/utils/industries.json';
+import { BiChevronDown } from 'react-icons/bi';
 
 const Navbar = () => {
    return (
       <div className='container'>
-         <nav className='flex items-center justify-between rounded-xl bg-blue-2 px-6 py-3 md:px-8'>
+         <nav className='flex items-stretch justify-between rounded-xl bg-blue-2 px-6 py-0 md:px-8'>
             <Link href='/'>
                <img
                   src={logo.src}
@@ -14,16 +16,31 @@ const Navbar = () => {
                   className='h-10 w-24 object-contain md:h-16 md:w-32'
                />
             </Link>
-            <ul className='hidden items-center gap-4 text-white md:flex'>
-               <Link href='/about'>
+            <ul className='hidden items-stretch gap-4 text-white md:flex md:gap-6'>
+               <Link className='flex items-center' href='/about'>
                   <li>About Us</li>
                </Link>
-               <li>Industry</li>
-               <li>Custom research</li>
-               <li>Company profiles</li>
-               <li>Reading</li>
+               <li className='dropdown-menu relative flex cursor-pointer items-center'>
+                  Industry <BiChevronDown className='inline text-xl' />
+                  <ul className='border-top absolute top-[100%] w-[320px] rounded-b-xl border border-s-400 bg-white font-medium text-s-800'>
+                     {industries.map((industry) => (
+                        <li className='cursor-pointer border-t border-s-400 px-6 py-2 font-medium hover:font-bold'>
+                           {industry}
+                        </li>
+                     ))}
+                  </ul>
+               </li>
+               <Link className='flex items-center' href='/reports'>
+                  <li>Custom research</li>
+               </Link>
+               <Link className='flex items-center' href='/reports'>
+                  <li>Company profiles</li>
+               </Link>
+               <Link className='flex items-center' href='/reports'>
+                  <li>Reading</li>
+               </Link>
             </ul>
-            <div>
+            <div className='flex items-center'>
                <Button>Become our client</Button>
             </div>
          </nav>
