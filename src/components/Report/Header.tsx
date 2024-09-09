94640e12f92d15d2b7bdee98401d5b1daeda291d
Adarsh <adarshmishra0701@gmail.com>
2024-09-10 01:03:18 +0530

    fix top position

--- src/components/Report/Header.tsx
+++ src/components/Report/Header.tsx
@@ -109,7 +109,7 @@ const Header: React.FC<{ data: any }> = ({ data }) => {
          </div>
          {showSecondHeader && (
             <div
-               className={`sticky left-0 right-0 top-[130px] z-20 border-b border-s-300 bg-white py-4 transition-all duration-300`}
+               className={`sticky left-0 right-0 top-[140px] z-20 border-b border-s-300 bg-white py-4 transition-all duration-300`}
                ref={headerRef2}
             >
                <div className='container'>
