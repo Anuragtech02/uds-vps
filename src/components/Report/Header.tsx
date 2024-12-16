6ca4cad60e3ba7859d3d33c655805304d3637627
Anurag Pal <apal895@gmail.com>
2024-12-16 18:33:43 +0530

    image changes

--- src/components/Report/Header.tsx
+++ src/components/Report/Header.tsx
@@ -210,43 +210,49 @@ const Header: React.FC<{ data: any }> = ({ data }) => {
             >
                <div className='container'>
                   <div className='flex flex-col items-start justify-between gap-4 md:flex-row md:items-center md:gap-6'>
-                     <div className='hidden md:flex flex-col lg:flex-row justify-start items-center gap-2'>
+                     <div className='hidden md:flex flex-col lg:flex-row justify-start items-center gap-2 lg:gap-10'>
                         <StrapiImage
                            media={{
-                              url: 'https://udsweb.s3.ap-south-1.amazonaws.com/uds_1921e7b253.svg',
-                              width: 50,
+                              url: 'https://udsweb.s3.ap-south-1.amazonaws.com/logo_dark_7353437088.svg',
+                              width: 120,
                               height: 50,
                            }}
                            objectFit='contain'
-                           className='!h-[50px] !object-contain lg:block hidden'
+                           className='!h-[25px] !object-contain lg:block hidden'
                         />
                         <div>
                            <h5 className='w-full overflow-hidden truncate text-ellipsis font-bold sm:text-wrap  lg:w-[100%]' dangerouslySetInnerHTML={{
-                              __html: reportHeaderData.title,
+                              __html: reportHeaderData.title?.split(":")?.[0] || reportHeaderData.title,
                            }}>
                            </h5>
                            <p className='text-xs'>Last Updated: {getMonthDate(reportHeaderData?.oldPublishedAt || reportHeaderData.updatedAt)}</p>
                         </div>
                      </div>
                      <div className='flex w-full items-center gap-2 md:max-w-max md:justify-end'>
-                        <LocalizedLink
-                           href={getCTALink(
-                              reportHeaderData.heroSectionPrimaryCTA.link,
-                           )}
-                           className='shrink grow basis-0 md:min-w-[200px] md:!max-w-[250px] md:shrink-0 md:grow-0 md:basis-[unset]'
-                        >
-                           <Button variant='secondary' className='w-full'>
-                              {reportHeaderData.heroSectionPrimaryCTA.title}
-                           </Button>
-                        </LocalizedLink>
-                        <div className='shrink grow basis-0 md:min-w-[200px] md:!max-w-[250px] md:shrink-0 md:grow-0 md:basis-[unset]'>
-                           <Button
-                              variant='light'
-                              className='w-full'
-                              onClick={handleBuyNow}
+                        <div className='text-right text-sm lg:block hidden'>
+                           <p>test@email.com</p>
+                           <p>+19787330253</p>
+                        </div>
+                        <div className='flex w-full items-center gap-2 md:max-w-max md:justify-end'>
+                           <LocalizedLink
+                              href={getCTALink(
+                                 reportHeaderData.heroSectionPrimaryCTA.link,
+                              )}
+                              className='shrink grow basis-0 md:min-w-[160px] md:!max-w-[250px] md:shrink-0 md:grow-0 md:basis-[unset]'
                            >
-                              {reportHeaderData.heroSectionSecondaryCTA.title}
-                           </Button>
+                              <Button variant='secondary' className='w-full'>
+                                 {reportHeaderData.heroSectionPrimaryCTA.title}
+                              </Button>
+                           </LocalizedLink>
+                           <div className='shrink grow basis-0 md:min-w-[160px] md:!max-w-[250px] md:shrink-0 md:grow-0 md:basis-[unset]'>
+                              <Button
+                                 variant='light'
+                                 className='w-full'
+                                 onClick={handleBuyNow}
+                              >
+                                 {reportHeaderData.heroSectionSecondaryCTA.title}
+                              </Button>
+                           </div>
                         </div>
                      </div>
                   </div>
