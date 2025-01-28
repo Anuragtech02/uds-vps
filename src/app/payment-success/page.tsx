028c09218bae1983bd8699db7446f879852fffb5
Anurag Pal <apal895@gmail.com>
2025-01-28 19:33:42 +0530

    rm unwanted import

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,6 +1,5 @@
 export const runtime = 'edge';
 import { LocalizedLink } from '@/components/commons/LocalizedLink';
-import Link from 'next/link';
 import { IoMdCheckmarkCircle } from 'react-icons/io';
 
 const PaymentSuccess = () => {
