7e8e6c9e8fb389d12bae4d14d6b8e13d9d7185c1
Anurag Pal <apal895@gmail.com>
2025-02-21 18:33:15 +0530

    sort in reverse order

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -128,7 +128,7 @@ const News = async ({ searchParams }: { searchParams: SearchParams }) => {
    const geographyFilters =
       searchParams.geographies?.split(',').filter(Boolean) || [];
    const currentPage = parseInt(searchParams.page || '1', 10);
-   const sortBy = searchParams.sortBy || 'relevance';
+   const sortBy = searchParams.sortBy || 'oldPublishedAt:desc';
 
    const filters = industryFilters.concat(geographyFilters);
    const filtersQuery = filters.reduce(
