0f346acbceb2de68beb5b996d4983430b1964c7d
Anurag Pal <apal895@gmail.com>
2025-02-24 15:44:07 +0530

    changes

--- src/utils/api/config.ts
+++ src/utils/api/config.ts
@@ -1,5 +1,5 @@
-const baseURL = process.env.API_URL || 'http://127.0.0.1:1337';
-const timeout = 10000;
+const baseURL = 'https://web-server-india.univdatos.com/api';
+const timeout = 50000;
 
 const fetchClient = async (url: string, options: any = {}) => {
    const fullUrl = `${baseURL}${url}`;
