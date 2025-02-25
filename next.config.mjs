9dec7f40f6c1a3b134689b9294a6cb019283067b
Anurag Pal <apal895@gmail.com>
2025-02-25 11:54:53 +0530

    try traefik redirects

--- next.config.mjs
+++ next.config.mjs
@@ -37,90 +37,90 @@ const nextConfig = {
          },
       ];
    },
-   async redirects() {
-      return [
-         {
-            source: '/reports/industry/:slug-market-research-reports',
-            destination: '/reports?industries=:slug',
-            permanent: true,
-         },
-         {
-            source: '/:locale/reports/industry/:slug-market-research-reports',
-            destination: '/:locale/reports?industries=:slug',
-            permanent: true,
-         },
-         {
-            source: '/reports/industry/:path*',
-            destination: '/reports?industries=:path*',
-            permanent: true,
-         },
-         {
-            source: '/:locale/reports/industry/:path*',
-            destination: '/:locale/reports?industries=:path*',
-            permanent: true,
-         },
-         {
-            source: '/report/:path*',
-            destination: '/reports/:path*',
-            permanent: true,
-         },
-         {
-            source: '/:locale/report/:path*',
-            destination: '/:locale/reports/:path*',
-            permanent: true,
-         },
-         {
-            source: '/custom-research',
-            destination: '/services',
-            permanent: true,
-         },
-         {
-            source: '/:locale/custom-research',
-            destination: '/:locale/services',
-            permanent: true,
-         },
-         {
-            source: '/reports/company-profile/:path*',
-            destination: '/',
-            permanent: true,
-         },
-         {
-            source: '/:locale/reports/company-profile/:path*',
-            destination: '/:locale/',
-            permanent: true,
-         },
-         {
-            source: '/terms-and-condition',
-            destination: '/terms-and-conditions',
-            permanent: true,
-         },
-         {
-            source: '/:locale/terms-and-condition',
-            destination: '/:locale/terms-and-conditions',
-            permanent: true,
-         },
-         // {
-         //    source: '/category/news/:slug',
-         //    destination: '/news?industries=:slug',
-         //    permanent: true,
-         // },
-         // {
-         //    source: '/:locale/category/news/:slug',
-         //    destination: '/:locale/news?industries=:slug',
-         //    permanent: true,
-         // },
-         // {
-         //    source: '/category/blog/:slug',
-         //    destination: '/blogs?industries=:slug',
-         //    permanent: true,
-         // },
-         // {
-         //    source: '/:locale/category/blog/:slug',
-         //    destination: '/:locale/blogs?industries=:slug',
-         //    permanent: true,
-         // },
-      ];
-   },
+   // async redirects() {
+   //    return [
+   //       {
+   //          source: '/reports/industry/:slug-market-research-reports',
+   //          destination: '/reports?industries=:slug',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/:locale/reports/industry/:slug-market-research-reports',
+   //          destination: '/:locale/reports?industries=:slug',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/reports/industry/:path*',
+   //          destination: '/reports?industries=:path*',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/:locale/reports/industry/:path*',
+   //          destination: '/:locale/reports?industries=:path*',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/report/:path*',
+   //          destination: '/reports/:path*',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/:locale/report/:path*',
+   //          destination: '/:locale/reports/:path*',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/custom-research',
+   //          destination: '/services',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/:locale/custom-research',
+   //          destination: '/:locale/services',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/reports/company-profile/:path*',
+   //          destination: '/',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/:locale/reports/company-profile/:path*',
+   //          destination: '/:locale/',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/terms-and-condition',
+   //          destination: '/terms-and-conditions',
+   //          permanent: true,
+   //       },
+   //       {
+   //          source: '/:locale/terms-and-condition',
+   //          destination: '/:locale/terms-and-conditions',
+   //          permanent: true,
+   //       },
+   //       // {
+   //       //    source: '/category/news/:slug',
+   //       //    destination: '/news?industries=:slug',
+   //       //    permanent: true,
+   //       // },
+   //       // {
+   //       //    source: '/:locale/category/news/:slug',
+   //       //    destination: '/:locale/news?industries=:slug',
+   //       //    permanent: true,
+   //       // },
+   //       // {
+   //       //    source: '/category/blog/:slug',
+   //       //    destination: '/blogs?industries=:slug',
+   //       //    permanent: true,
+   //       // },
+   //       // {
+   //       //    source: '/:locale/category/blog/:slug',
+   //       //    destination: '/:locale/blogs?industries=:slug',
+   //       //    permanent: true,
+   //       // },
+   //    ];
+   // },
 };
 
 // if (process.env.NODE_ENV === 'development') {
