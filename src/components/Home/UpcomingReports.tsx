f8fb48a06f97c2cbc65d65fdac9e864300526d1a
Anurag Pal <apal895@gmail.com>
2025-03-17 16:35:27 +0530

    optimize slider

--- src/components/Home/UpcomingReports.tsx
+++ src/components/Home/UpcomingReports.tsx
@@ -8,8 +8,52 @@ import CustomResearchCTA from '../commons/CustomResearchCTA';
 import { SLICK_COMMON_SETTINGS } from '@/utils/constants';
 
 const UpcomingReports: React.FC<{ data: any }> = ({ data }) => {
-   const settings = SLICK_COMMON_SETTINGS;
+   const settings = {
+      ...SLICK_COMMON_SETTINGS,
+      slidesToShow: 4,
 
+      responsive: [
+         {
+            breakpoint: 1200,
+            settings: {
+               slidesToShow: 4,
+               // slidesToScroll: 1,
+               infinite: true,
+               dots: false,
+            },
+         },
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 3,
+               // slidesToScroll: 1,
+               infinite: true,
+               dots: false,
+            },
+         },
+         {
+            breakpoint: 786,
+            settings: {
+               slidesToShow: 2,
+               // slidesToScroll: 2,
+            },
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               // slidesToScroll: 2,
+            },
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               slidesToShow: 1,
+               // slidesToScroll: 1,
+            },
+         },
+      ],
+   };
    return (
       data?.upcomingReports?.length > 0 && (
          <section className='block min-h-max pb-12 md:pb-20'>
